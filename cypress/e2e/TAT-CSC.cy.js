acc993daf573fa84f221f2d0f17ce25ecc8b753d
ZenurikSD <alissonsilva498@outlook.com.br>
2024-09-01 18:58:38 -0300

    Complete Exercise 1

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -9,13 +9,24 @@ describe('TAT Customer Service Center', () => {
   })
 
   it('fills in the required fields and submits the form', () => {
-    //Arrange
+    //Arrange - seria "chegar até a página", mas já incluí no beforeEach()
 
     //Act
-    cy.get('#firstName').type('Alisson')
-    cy.get('#lastName').type('Silva')
-    cy.get('#email').type('alissonsilva@email.com')
+    cy.get('#firstName').as('first').type('Alisson')
+    cy.get('#lastName').as('last').type('Silva')
+    cy.get('#email').as('email').type('alissonsilva@email.com')
+    cy.get('#open-text-area').as('txtarea').type('Test with .type() and .click()!', {delay: 0})
+    
+    //Assert 1
+    cy.get('@first').should('have.value', 'Alisson')
+    cy.get('@last').should('have.value', 'Silva')
+    cy.get('@email').should('have.value', 'alissonsilva@email.com')
+    cy.get('@txtarea').should('have.value', 'Test with .type() and .click()!')
 
-    //Assert
+    //Act 2 -- Nada diz que eu não posso encadear o AAA 🤔
+    cy.get('.button').contains('Send').click()
+
+    //Assert 2
+    cy.get('.success').should('be.visible')
   })
 })
