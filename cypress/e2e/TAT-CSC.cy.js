44886a6b1b079d73977f17029fd69d2829553668
ZenurikSD <alissonsilva498@outlook.com.br>
2024-09-02 19:23:22 -0300

    Extra exercise 2

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -2,6 +2,12 @@ describe('TAT Customer Service Center', () => {
 
   beforeEach(() => {
     cy.visit('./src/index.html')
+
+    //Isso é uma boa prática? 
+    cy.get('#firstName').as('first')
+    cy.get('#lastName').as('last')
+    cy.get('#email').as('email')
+    cy.get('#open-text-area').as('txtarea')
   })
 
   it('checks app page title', () => {
@@ -12,10 +18,10 @@ describe('TAT Customer Service Center', () => {
     //Arrange - seria "chegar até a página", mas já incluí no beforeEach()
 
     //Act
-    cy.get('#firstName').as('first').type('Alisson')
-    cy.get('#lastName').as('last').type('Silva')
-    cy.get('#email').as('email').type('alissonsilva@email.com')
-    cy.get('#open-text-area').as('txtarea').type('Test with .type() and .click()!', {delay: 0})
+    cy.get('@first').type('Alisson')
+    cy.get('@last').type('Silva')
+    cy.get('@email').type('alissonsilva@email.com')
+    cy.get('@txtarea').type('Test with .type() and .click()!', {delay: 0})
     
     //Assert 1
     cy.get('@first').should('have.value', 'Alisson')
@@ -28,5 +34,16 @@ describe('TAT Customer Service Center', () => {
 
     //Assert 2
     cy.get('.success').should('be.visible')
+  })
+
+  it('displays an error mesage when submitting the form with an email with invalid formatting', () => {
+    //Poderia mover isso pra um custom command..
+    cy.get('@first').type('Alisson')
+    cy.get('@last').type('Silva')
+    cy.get('@email').type('alissonsilva')
+    cy.get('@txtarea').type('Invalid email test')
+
+    cy.get('.button').contains('Send').click()
+    cy.get('.error').should('be.visible')    
   })
 })
